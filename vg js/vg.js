fdcc0b306bc81531c41fd09ef2ae9481b0f9ee0e
Colin_Yip <cyip0011@student.monash.edu>
2026-05-09 20:14:34 +0800

    Update file paths in Vega scripts and JSON configurations for data accessibility

--- vg js/vg.js
+++ vg js/vg.js
@@ -1,4 +1,4 @@
-var vg_1 = "ridership_flowmap.vg.json";
+var vg_1 = "vg json/ridership_flowmap.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
